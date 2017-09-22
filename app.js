c889f1b06355f6ef91f8c6e7f1990233be75d88a
yshuman1 <yshuman@loyalloops.com>
2017-09-22 15:22:16 -0700

    removed arrays, learned to access key value pairs from return object. calculated time since midnight and compared to time elapsed for each prayer to figure out which salat was next using if else. then recalculated for when next prayer is the following day

--- app.js
+++ app.js
@@ -1,62 +1,84 @@
-
 var request = require('request');
 
 
 var zip = '94513';
 var method = 2;
 
-request(`https://api.aladhan.com/timingsByAddress?address=${zip}&method=${method}`, function(error, response, body){
-var parsed = JSON.parse(body);
-var imsak = parsed.data.timings.Imsak;
-var fajr = parsed.data.timings.Fajr;
-var sunrise = parsed.data.timings.Sunrise;
-var dhuhr = parsed.data.timings.Dhuhr;
-var asr = parsed.data.timings.Asr;
-var maghrib = parsed.data.timings.Maghrib;
-var isha = parsed.data.timings.Isha;
+request(`https://api.aladhan.com/timingsByAddress?address=${zip}&method=${method}`, function (error, response, body) {
+  var parsed = JSON.parse(body);
+  var timings = parsed.data.timings;
+  var currentHour = new Date().getHours();
+  var currentMinutes = new Date().getMinutes();
+  var minutesSinceMidnight = currentHour * 60 + currentMinutes;
 
-//can i put all these times in an array instead of separate var's?
-console.log(`Imsak: ${imsak}`)
-console.log(`Fajr: ${fajr}`);
-console.log(`Sunrise: ${sunrise}`);
-console.log(`Dhuhr: ${dhuhr}`);
-console.log(`Asr: ${asr}`);
-console.log(`Maghrib: ${maghrib}`);
-console.log(`Isha: ${isha}`);
+  console.log('\n    Salat Times    ');
+  console.log('*******************');
+  console.log(`Fajr: ${timings.Fajr}`);
+  console.log(`Dhuhr: ${timings.Dhuhr}`);
+  console.log(`Asr: ${timings.Asr}`);
+  console.log(`Maghrib: ${timings.Maghrib}`);
+  console.log(`Isha: ${timings.Isha}`);
+  console.log('*******************');
+  console.log(`Current time: ${currentHour}:${currentMinutes}`);
 
-var currentHour = new Date().getHours();
-var currentMinutes = new Date().getMinutes();
-var fajrHour = fajr.substr(0,2);
-var fajrMinute = fajr.substr(3,5);
-//todo: fix this
-function timeLeft(){
-  a = 57;
-  b = fajrMinute;
-  console.log(parseInt(b)-parseInt(a));
-  console.log(fajrHour-1);
-  
-  //how can you calculate which salat is next?
-  //that way you can set that as a var which is used to calc remaining time.
-  
-  
-}
-timeLeft();
-console.log(``);
+
+  var minutes = {};
+  for (var k in timings) {
+    var hour = timings[k].substr(0, 2);;
+    var minute = timings[k].substr(3, 5);
+    minutes[k] = hour * 60 + parseInt(minute);
+  };
 
 
 
-// var hoursLeft = parseInt(fajrHour) - parseInt(currentHour);
-// var hourDiff = (parseInt(fajrHour) - parseInt(currentHour))*60;
-// var minuteDiff = 60-parseInt(currentMinutes) + parseInt(fajrMinute);
-// var timediff = (hourDiff+minuteDiff)%60;
-// console.log(`fajr time: ${fajrHour}:${fajrMinute}`);
+  var next = "";
 
-// console.log(`current time: ${currentHour}:${currentMinutes}\n hour diff: ${hourDiff} minute diff: ${minuteDiff} timeDiff: ${timediff} hoursleft: ${hoursLeft}`);
+  if (minutes.Fajr > minutesSinceMidnight) {
+    var next = 'Fajr';
 
-//take time and figure out which salat is next 
-//salat times  exclude imsak & sunrise
-//figure out how many hours and minutes left until next salat time
+  } else if (minutes.Dhuhr > minutesSinceMidnight) {
+    var next = 'Duhr';
+
+  } else if (minutes.Asr > minutesSinceMidnight) {
+    var next = 'Asr';
+
+  } else if (minutes.Maghrib > minutesSinceMidnight) {
+    var next = 'Maghrib';
+
+  } else if (minutes.Isha > minutesSinceMidnight) {
+    var next = 'Isha';
+
+  } else {
+    var tomorrow = Date.now() / 1000;
+    request(`https://api.aladhan.com/timingsByAddress/${tomorrow}?address=${zip}&method=${method}`, function (error, response, body) {
+      var parsed = JSON.parse(body);
+      var Fajr = parsed.data.Fajr;
+      var hour = Fajr.substr(0, 2);;
+      var minute = Fajr.substr(3, 5);
+      //1440 is how minutes in a day
+      var timeLeft = 1440 - minutesSinceMidnight + (60 * hour) + parseInt(minute);
+      var hour = Math.floor(timeLeft / 60);
+      var minute = timeLeft % 60;
+      if (minute < 10) {
+        minute = "0" + minute;
+      } else {
+        minute = parseInt(minute);
+      }
+
+      console.log(`Time remaining until ${next}: ${hour}:${minute}`);
+
+    });
+  }
 
 
-  
+  var timeLeft = minutes[next] - minutesSinceMidnight;
+  var hour = Math.floor(timeLeft / 60);
+  var minute = timeLeft % 60;
+  if (minute < 10) {
+    minute = "0" + minute;
+  } else {
+    minute = parseInt(minute);
+  }
+
+  console.log(`Time remaining until ${next}: ${hour}:${minute} \n`);
 });
